039251631d9d634bdca6e48b000fd82c6877ebc6
ubuntu15.04 <kltdaer@gmail.com>
2016-05-27 01:35:18 -0700

    version 0.1

--- Data-S.py
+++ Data-S.py
@@ -16,21 +16,27 @@ def push_to_database():
 								  host='192.168.253.133',
 								  database='data')
 	cursor = cnx.cursor()
-	insert_data = ("INSERT INTO data_record "
-			  "(id, state, date) "
-			  "VALUES (%(id)s, %(state)s, %(date)s")
+	insert_data = ("INSERT INTO wuxi "
+			  "(factory_id, gate_id, node_id, record_time, parameter_type1, parameter_value1, parameter_type2, parameter_value2, parameter_type3, parameter_value3) "
+			  "VALUES (%(factory_id)s, %(gate_id)s, %(node_id)s, FROM_UNIXTIME(%(record_time)s), %(parameter_type1)s, %(parameter_value1)s, %(parameter_type2)s, %(parameter_value2)s, %(parameter_type3)s, %(parameter_value3)s)")
 	global data_buffer
 	while True:
 		con.acquire()
 		if len(data_buffer) >= MIN_SIZE:
+			print('db-thread will transport')
 			copy_data = copy.deepcopy(data_buffer)
 			data_buffer = []	
 			con.release()
 			for data in copy_data:
-				cursor.execute(insert_data, data)
-			cursor.commit()
+				try:
+					cursor.execute(insert_data, data)
+				except Exception, e:
+					print e
+			cnx.commit()
 		else:
+			print('db-thread will wait')
 			con.wait()
+			con.release()
 
 
 def gather_in_buffer():
@@ -46,31 +52,35 @@ def gather_in_buffer():
 		while True:
 			events = epoll.poll(1)
 			for fileno, event in events:
+				#print('event:' + str(fileno))
 				if fileno == serversocket.fileno():
 					connection, address = serversocket.accept()
 					connection.setblocking(0)
 					epoll.register(connection.fileno(), select.EPOLLIN)
 					connections[connection.fileno()] = connection
+					print('Add a connection: ' + str(connection.fileno()))
 				elif event & select.EPOLLIN:	 #读数据 如果完整直接入buffer，不完整等下次读完整再入  TODO：如何知道当前流已读完？
+					#print('Receiving msg: ' + str(fileno))	
 					if not res.has_key(fileno):
 						res[fileno] = ''
 					while True:
 						cur_data = connections[fileno].recv(1024)
-						res[fileno] += cur_data
-						if not cur_data:
+						#print 'cur_data:' + cur_data
+						res[fileno] += cur_data	
+						if res[fileno][-4:] == '####':
 							break
-					if res[fileno][-4:] == '####':
-						con.acquire()
-						data_buffer.append(pickle.loads(res[fileno][:-4]))
-						del res[fileno]
-						if len(data_buffer) == MIN_SIZE:
-							con.notify()
-						else:
-							con.release()
-					elif event & select.EPOLLHUP:
-						epoll.unregister(fileno)
-						connections[fileno].close()
-						del connections[fileno]
+					con.acquire()
+					data_buffer.append(pickle.loads(res[fileno][:-4]))
+					#print('Receiving')
+					del res[fileno]
+					if len(data_buffer) == MIN_SIZE:
+						print('notify')
+						con.notify()
+					con.release()
+				elif event & select.EPOLLHUP:
+					epoll.unregister(fileno)
+					connections[fileno].close()
+					del connections[fileno]
 	finally:
 		epoll.unregister(serversocket.fileno())
 		epoll.close()
--- robot.py
+++ robot.py
@@ -4,76 +4,92 @@ import random
 import pickle
 import socket
 import time
+
+DATA_S_IP = '127.0.0.1'
+DATA_S_PORT = 8484
+
 TYPE_AMOUNT = 10
 PORT_TUPLE = (19455,19456,19457,19458,19568,19569,19570,19571,19572,19573)
 data_detail = {
-    'factory_id' : 0,
-    'gate_id' : 0,
-    'node_id' : 0,
-    'record_time' : 0,
-    'parameter_type1' : 0,
-    'parameter_value1' : 0,
-    'parameter_type2' : 0,
-    'parameter_value2' : 0,
-    'parameter_type3' : 0,
-    'parameter_value4' : 0,
-    }
+	'factory_id' : 0,
+	'gate_id' : 0,
+	'node_id' : 0,
+	'record_time' : 0,
+	'parameter_type1' : 0,
+	'parameter_value1' : 0,
+	'parameter_type2' : 0,
+	'parameter_value2' : 0,
+	'parameter_type3' : 0,
+	'parameter_value4' : 0,
+	}
 
 class Node:
-    last_time = 0
-    def __init__(self, sock, factory_id, gate_id, node_id, interval):
-        self.sock = sock
-        self.gate_id = gate_id
-        self.node_id = node_id
-        self.type1 = random.randint(1,5)
-        self.type2 = random.randint(6,10)
-        self.type3 = random.randint(11,15)
-        self.interval = interval
-        
-    def update(self, time):
-        if(time >= last_time + interval):
-            self.send_info(time)
+	last_time = 0
+	def __init__(self, sock, factory_id, gate_id, node_id, interval):
+		self.sock = sock
+		self.factory_id = factory_id
+		self.gate_id = gate_id
+		self.node_id = node_id
+		self.type1 = random.randint(1,5)
+		self.type2 = random.randint(6,10)
+		self.type3 = random.randint(11,15)
+		self.interval = interval
+		
+	def update(self, time):
+		if(time >= self.last_time + self.interval):
+			self.send_info(time)
+			return True
+		else:
+			return False
 
-    def send_info(self, time):
-        data_detail.factory_id = self.factory_id
-        data_detail.gate_id = self.gate_id
-        data_detail.node_id = self.node_id
-        data_detail.parameter_type1 = self.type1
-        data_detail.parameter_value1 = random.randint(1,100)
-        data_detail.parameter_type2 = self.type2
-        data_detail.parameter_value2 = random.randint(1,100)
-        data_detail.parameter_type3 = self.type3
-        data_detail.parameter_value3 = random.randint(1,100)
-        data_detail.record_time = time
-        self.last_time = time
-        info = pickle.dumps(data_detail)
-        info += '####'
-        self.sock.send(info)
+	def send_info(self, time):
+		data_detail['factory_id'] = self.factory_id
+		data_detail['gate_id'] = self.gate_id
+		data_detail['node_id'] = self.node_id
+		data_detail['parameter_type1'] = self.type1
+		data_detail['parameter_value1'] = random.randint(1,100)
+		data_detail['parameter_type2'] = self.type2
+		data_detail['parameter_value2'] = random.randint(1,100)
+		data_detail['parameter_type3'] = self.type3
+		data_detail['parameter_value3'] = random.randint(1,100)
+		data_detail['record_time'] = time
+		self.last_time = time
+		info = pickle.dumps(data_detail)
+		info += '####'
+		self.sock.send(info)
 
 class Gate:
-    nodes = []
-    def __init__(self, gate_id, port):
-        self.gate_id = gate_id
-        self.port = port
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind('127.0.0.1', port)
-        self.sock = sock
-        for i in range(1,random.randint(30,50)) do:
-            self.nodes[i] = Node(sock, gate_id, i, randint(20,30))
-            
+	nodes = []
+	def __init__(self, factory_id, gate_id, port):
+		self.gate_id = gate_id
+		self.port = port
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.bind(('127.0.0.1', port))
+		sock.connect((DATA_S_IP, DATA_S_PORT))
+		self.sock = sock
+		for i in range(1,random.randint(30,50)):
+			self.nodes.append(Node(sock, factory_id, gate_id, i, random.randint(20,30)))
+		self.nodes_amount = len(self.nodes)
+			
 if __name__ == '__main__':
-    gates = []
-    num = 1
-    for port in PORT_TUPLE:
-        gate = Gate(num, port)
-        num += 1
-    while True:
-        begin_time = time.time()
-        for gate in gates:
-            for node in gate.nodes:
-                node.update(int(begin_time))
-        end_time = time.time
-        duration = end_time - begin_time
-        if duration < 1:
-            time.sleep(1 - duration)
-        
+	gates = []
+	port = 20003
+	total_nodes_amount = 0
+	for i in range(1,20):
+		for j in range(1,random.randint(1,6)):
+			gates.append(Gate(i, j, port))
+			port += 1
+			total_nodes_amount += gates[-1].nodes_amount
+	while True:
+		begin_time = time.time()
+		nodes_amount = 0
+		for gate in gates:
+			for node in gate.nodes:
+				if node.update(int(begin_time)):
+					nodes_amount += 1
+		end_time = time.time()
+		duration = end_time - begin_time
+		print 'UPDATE_NODES_AMOUNT:' + str(nodes_amount)
+		if duration < 1:
+			time.sleep(1 - duration)
+		
